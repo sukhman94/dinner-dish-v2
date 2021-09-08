16e38b82f6097b6be62a19f75381f28d08e0df58
Adan099 <adan.sarfraz@devsinc.com>
2021-09-08 20:44:25 +0500

    Add production mailing url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'dinnerdashv2.herokuapp.com/' }
   # change when on production
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
