4fcc5c93232c2e11227d5746667cf554217eb882
Adan099 <adan.sarfraz@devsinc.com>
2021-09-09 10:50:37 +0500

    updated heroku error

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,8 +2,9 @@
 
 module ItemsHelper
   def item_avatar(item)
+    size = '100%x100%!'
     if item.item_image[0]
-      image_tag item.item_image[0]
+      image_tag item.item_image[0].variant(resize: size).processed
     else
       image_tag 'default_item.png', class: 'w-100'
     end
